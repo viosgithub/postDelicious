0c729667c419d64853eecf6eb5fc7cc516d62a25
vios@Hydrogen <viospj@gmail.com>
2011-03-10 18:59:36 +0900

    added tag and comment posting

--- README.txt
+++ README.txt
@@ -13,6 +13,27 @@ Deliciousにブックマークを投稿します
 コメントにsomething
 を加えることができます．
 
+TODO:
+タグを,ではなくスペースでパースして
+投稿できるようにする
+
+Oauth認証を実装してYahooアカウントでも
+ポストできるようにする
+
+パスワード入力時は***に変更したい
+
+INSTALL:
+windows ホームフォルダ\vimperator\plugin\の中に突っ込む
+(.vimperator)ではないので注意
+
+Linux系　~/.vimperator/plugin/以下にどうぞ
+
+Firefixを再起動すると使えるようになる
+一応起動中に :so　コマンドでプラグインを再読み込みできないでもない
+
+USE:
+postD or postDelicious
+で投稿可能．オプションは上記の例を参考のこと
 
 
 
--- postdelicious.js
+++ postdelicious.js
@@ -1,42 +1,48 @@
+var name = "";
+var pass = "";
 commands.addUserCommand(
-    ["postD[elicious]"],
-    "this plugin post Delicious your bookmark",
-    function(args){
-    	var c = gBrowser.selectedBrowser.contentDocument.documentURI;
-	var name = "";
-	var pass = "";
+	["postD[elicious]"],
+	"this plugin post Delicious your bookmark",
+	function(args){
+	var pageUrl = gBrowser.selectedBrowser.contentDocument.documentURI;
 	if (name == "") name = prompt("Delicious ID","");
 	if (pass == "") pass = prompt("Delicious PASS","");
-        liberator.echo(args["-tags"])
-	liberator.echo(args["-tags"].length)
-        //liberator.echo(args["tags"])
-        //liberator.echo("this is foo2!!");
-	var title = gBrowser.selectedBrowser.contentTitle;
-        //liberator.echo("comment is " + args["-comment"]);
+	liberator.echo("tags:" + args["-tags"])
+	var pageTitle = gBrowser.selectedBrowser.contentTitle;
+	liberator.echo("comment:" + args["-comment"]);
 	if (args["-comment"] == null) liberator.echo("comment is undefind by mori");
-        liberator.echo("title="+title);
-	var url = "https://api.del.icio.us/v1/posts/add?";
-	url = url + "url=" + c;
-	url = url + "&description=" + title;
-        liberator.echo(url);
-	//var request = createXMLHttpRequest();
+	liberator.echo("title="+pageTitle);
+	var postUri = "https://api.del.icio.us/v1/posts/add?";
+	postUri = postUri + "url=" + encodeURI(pageUrl);
+	postUri = postUri + "&description=" + encodeURI(pageTitle);
+	if(args["-comment"] != null) postUri = postUri + "&extended=" + encodeURI(args["-comment"]);
+	var tags = args["-tags"];
+	if(tags != null) 
+	{
+	postUri = postUri + "&tags=";
+	for(var i=0;i<tags.length;i++)
+	{
+	    postUri = postUri + encodeURI(args["-tags"][i] + " ");
+	}
+	}
+	liberator.echo("encoded postUri:" + postUri);
 	var request = new XMLHttpRequest();
-        liberator.echo("createXML...:OK");
-	request.open("POST",url,false,name,pass);
-        liberator.echo("request.open:OK");
-  	request.setRequestHeader('Content-Type', 'application/post2delplugin');
-        liberator.echo("setRequestHeader:OK");
+	liberator.echo("createXML...:OK");
+	request.open("GET",postUri,false,name,pass);
+	liberator.echo("request.open:OK");
+	request.setRequestHeader('Content-Type', 'application/post2delplugin');
+	liberator.echo("setRequestHeader:OK");
 	request.send("");
-        liberator.echo("send:OK");
-        liberator.echo(request.status);
+	if(request.status == 200) liberator.echo("succsessd!");
+	else liberator.echo("failed!");
 
-    },
-    {
+	},
+{
 options: [
-  [["-comment",   "-c"], commands.OPTION_STRING],
-  [["-tags",    "-t"], commands.OPTION_LIST]
-  ]
-  }
-  ,
-  true
-);
+	     [["-comment",   "-c"], commands.OPTION_STRING],
+	 [["-tags",    "-t"], commands.OPTION_LIST]
+	     ]
+}
+,
+    true
+    );
