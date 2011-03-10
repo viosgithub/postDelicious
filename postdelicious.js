5e59ed2ed85e8873893b90bae392841bbf0ea49b
vios@Hydrogen <viospj@gmail.com>
2011-03-11 03:33:40 +0900

    remove basic auth

--- postdelicious.js
+++ postdelicious.js
@@ -1,41 +1,32 @@
-var name = "";
-var pass = "";
 commands.addUserCommand(
-	["postD[elicious]"],
-	"this plugin post Delicious your bookmark",
+	["postD[elicious]"],//コマンド名
+	"this plugin posts Delicious your bookmark",//コマンドに関する説明，コマンドラインに表示される
 	function(args){
-	var pageUrl = gBrowser.selectedBrowser.contentDocument.documentURI;
-	if (name == "") name = prompt("Delicious ID","");
-	if (pass == "") pass = prompt("Delicious PASS","");
-	liberator.echo("tags:" + args["-tags"])
-	var pageTitle = gBrowser.selectedBrowser.contentTitle;
-	liberator.echo("comment:" + args["-comment"]);
-	if (args["-comment"] == null) liberator.echo("comment is undefind by mori");
-	liberator.echo("title="+pageTitle);
-	var postUri = "https://api.del.icio.us/v1/posts/add?";
-	postUri = postUri + "url=" + encodeURI(pageUrl);
-	postUri = postUri + "&description=" + encodeURI(pageTitle);
-	if(args["-comment"] != null) postUri = postUri + "&extended=" + encodeURI(args["-comment"]);
-	var tags = args["-tags"];
+	var pageUrl = gBrowser.selectedBrowser.contentDocument.documentURI;//投稿したいページのURI(URL)の取得
+	//liberator.echo("tags:" + args["-tags"]);//入力されたタグ一覧の表示．デバッグ用
+	var pageTitle = gBrowser.selectedBrowser.contentTitle;//投稿するページのタイトルの取得
+	//liberator.echo("comment:" + args["-comment"]);//入力されたコメントの表示．デバッグ用
+	var postUri = "https://api.del.icio.us/v1/posts/add?";//DeliciousのAPIの使うためのURIの先頭部分，ここに&tags,&urlとかを追加して送信することでAPIを実行させる
+	postUri = postUri + "url=" + encodeURI(pageUrl);//URIの追加
+	postUri = postUri + "&description=" + encodeURI(pageTitle);//タイトルの追加
+	if(args["-comment"] != null) postUri = postUri + "&extended=" + encodeURI(args["-comment"]);//もしコメントオプションに記述があればコメント（extended)を追加
+	var tags = args["-tags"];//オプションで入力されたタグを tags　に代入
 	if(tags != null) 
 	{
-	postUri = postUri + "&tags=";
+	postUri = postUri + "&tags=";//タグが入力されていればタグの追加を行なうので，&tagsを追加
 	for(var i=0;i<tags.length;i++)
 	{
-	    postUri = postUri + encodeURI(args["-tags"][i] + " ");
+	    postUri = postUri + encodeURI(args["-tags"][i] + " ");//tags[1] tags[2] tags[n] となるように取得したタグを&tags以降に続けていく
 	}
 	}
-	liberator.echo("encoded postUri:" + postUri);
-	var request = new XMLHttpRequest();
-	liberator.echo("createXML...:OK");
-	request.open("GET",postUri,false,name,pass);
-	liberator.echo("request.open:OK");
-	request.setRequestHeader('Content-Type', 'application/post2delplugin');
-	liberator.echo("setRequestHeader:OK");
-	request.send("");
-	if(request.status == 200) liberator.echo("succsessd!");
-	else liberator.echo("failed!");
-
+	//liberator.echo("encoded postUri:" + postUri);//最終的に送信するURIの表示．デバッグ用
+	var request = new XMLHttpRequest();//XMLHttpRequestを使って通信を行なう
+	//request.open("GET",postUri,false,name,pass);//第4,5引数はbasic認証に必要となる．第1引数を"POST"に変更するとPOSTメソッドを用いる．第３引数をtrueにすると非同期通信を行なう
+	request.open("GET",postUri,false);//第1引数を"POST"に変更するとPOSTメソッドを用いる．第３引数をtrueにすると非同期通信を行なう
+	request.setRequestHeader('Content-Type', 'application/post2delplugin');//第２引数の設定をどうするのか良く分からないで勝手に決めた.deliciousAPIヘルプにユニークな名前を使うべき的なことが書いてあったような...
+	request.send("");//リクエストを送信する
+	if(request.status == 200) liberator.echo("succsessd!");//通信に成功したら200が返ってくるので成功したことを表示する
+	else liberator.echo("failed!");//失敗したときはこっちを表示する
 	},
 {
 options: [
